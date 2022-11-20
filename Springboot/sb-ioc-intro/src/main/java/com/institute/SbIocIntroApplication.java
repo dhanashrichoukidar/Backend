f802c8feaad53e10508c05dbe48a3cf8784a798d
dhanashrichoukidar <choukidar.dhanashree805@gmail.com>
2022-11-20 17:39:32 +0530

    Springboot Programs

---
+++ Springboot/sb-ioc-intro/src/main/java/com/institute/SbIocIntroApplication.java
@@ -0,0 +1,13 @@
+package com.institute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbIocIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbIocIntroApplication.class, args);
+	}
+
+}
